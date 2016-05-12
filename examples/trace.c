141d7724a9a36eb4b86aaa56bdf9b95f50a61e10
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-05-12 08:47:36 +0200

    Trace events demo software

--- examples/trace.c
+++ examples/trace.c
@@ -7,12 +7,27 @@
 
 #define STM_TRACE(id, value) \
   { \
-    register uintptr_t v asm("x10") = value; \
-    asm volatile ("" ::: "memory");		\
+    asm volatile ("mv   a0,%0": :"r" ((uint64_t)value) : "a0");	\
     asm volatile ("csrw 0x8f0, %0" :: "r"(id));	\
   }
 
+static void trace_event0() {
+  STM_TRACE(0, 0x42);
+}
+
+static void trace_event1(uint64_t value) {
+  STM_TRACE(1, value);
+}
+
+static void trace_event2(uint64_t id, uint64_t value) {
+  STM_TRACE(id, value);
+}
+
 int main() {
   STM_TRACE(0x1234, 0xdeadbeef);
+
+  trace_event0();
+  trace_event1(23);
+  trace_event2(0xabcd, 0x0123456789abcdef);
 }
 
