0839ae57db5011b02f4f04e60f903e11a38d151a
Wei Song <wsong83@gmail.com>
2016-07-01 16:46:48 +0100

    update bare-metal examples, boot does not work, trace not tested

--- driver/elf.c
+++ driver/elf.c
@@ -25,7 +25,7 @@
  * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  *
  *----------------------------------------------------------------------------
- * Copyright (c) 2015, University of Cambridge.
+ * Copyright (c) 2015-2016, University of Cambridge.
  * All Rights Reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -52,69 +52,9 @@
  * MODIFICATIONS.
  *----------------------------------------------------------------------------*/
 
-#include "elf.h"
-
+#include <elf.h>
 #include <string.h>
 
-#define IS_ELF(hdr) \
-  ((hdr).e_ident[0] == 0x7f && (hdr).e_ident[1] == 'E' && \
-   (hdr).e_ident[2] == 'L'  && (hdr).e_ident[3] == 'F')
-
-#define IS_ELF64(hdr) (IS_ELF(hdr) && (hdr).e_ident[4] == 2)
-
-#define PT_LOAD 1
-
-#define SHT_NOBITS 8
-
-typedef struct {
-  uint8_t  e_ident[16];
-  uint16_t e_type;
-  uint16_t e_machine;
-  uint32_t e_version;
-  uint64_t e_entry;
-  uint64_t e_phoff;
-  uint64_t e_shoff;
-  uint32_t e_flags;
-  uint16_t e_ehsize;
-  uint16_t e_phentsize;
-  uint16_t e_phnum;
-  uint16_t e_shentsize;
-  uint16_t e_shnum;
-  uint16_t e_shstrndx;
-} Elf64_Ehdr;
-
-typedef struct {
-  uint32_t sh_name;
-  uint32_t sh_type;
-  uint64_t sh_flags;
-  uint64_t sh_addr;
-  uint64_t sh_offset;
-  uint64_t sh_size;
-  uint32_t sh_link;
-  uint32_t sh_info;
-  uint64_t sh_addralign;
-  uint64_t sh_entsize;
-} Elf64_Shdr;
-
-typedef struct {
-  uint32_t p_type;
-  uint32_t p_flags;
-  uint64_t p_offset;
-  uint64_t p_vaddr;
-  uint64_t p_paddr;
-  uint64_t p_filesz;
-  uint64_t p_memsz;
-  uint64_t p_align;
-} Elf64_Phdr;
-
-typedef struct {
-  uint32_t st_name;
-  uint8_t  st_info;
-  uint8_t  st_other;
-  uint16_t st_shndx;
-  uint64_t st_value;
-  uint64_t st_size;
-} Elf64_Sym;
 
 int load_elf(uint8_t *target_base, const uint8_t *elf, const uint32_t elf_size) {
   // sanity checks
